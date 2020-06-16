6daff8282f16700c99698215d67c7885d4003b17
Inou Ridder <inouridder@gmail.com>
2020-06-16 16:59:11 +0200

    ft: rename and more comments for explaining the pattern

--- responderPattern.php
+++ responderPattern.php
@@ -1,4 +1,10 @@
 <?php 
+// The main idea for this pattern is to clean up controller actions and extract logic into their own contained classes which are easy to unit test.
+// The controller action should only call well named methods, so that the language is very clear for the user.
+// the actions should live in an object called a responder, which responds to a request.
+
+// The main idea is to use the magic method __call to catch any method call to $this and check if the method called perhaps lives in the responder belonging
+// to that controller action, if it does, then it calls that method and returns the result.
 
 // filepath: App\Responders\Responders
 trait Responders {
@@ -32,10 +38,6 @@ trait Responders {
         // In laravel this would be a custom laravel error
         throw new Exception("Method does not exist");
     }
-    
-    protected function respondWith($args) {
-        return ['response' => $args];
-    }
 
     private function currentAction() 
     {
@@ -52,12 +54,12 @@ trait Responders {
 class UserControllerIndexResponder {
     public function fetchUsers() 
     {
-        echo('FETCHING ALL THE USERS');
+        echo("FETCHING ALL THE USERS \n");
     }
 
     public function sanitizeUsers($users = []) 
     {
-        echo('SANITIZING USERS');
+        echo("SANITIZING USERS \n");
     }
 }
 
@@ -92,7 +94,15 @@ class UserControllerUpdateResponder {
     }
 }
 
-class UserController {
+class BaseController {
+    
+    // sanity check to see if it works with inheritance
+    protected function respondWith($args) {
+        return ['response' => $args];
+    }
+}
+
+class UserController extends BaseController {
     use Responders;
 
     public function index() 
@@ -137,4 +147,7 @@ class UserController {
 $controller = new UserController;
 
 $controller->index();
+// logs:
+// FETCHING ALL THE USERS
+// SANITIZING USERS
 
